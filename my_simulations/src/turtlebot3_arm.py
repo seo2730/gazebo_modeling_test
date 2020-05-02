ceab4defdde648066ca92ee5d59628c38df124cd
seo2730 <42115807+seo2730@users.noreply.github.com>
2020-05-02 17:44:00 +0900

    modeling arm completed, simulation testing
    simulation testing 90% completed
    robot model inertial problem have to solve

--- my_simulations/src/turtlebot3_arm.py
+++ my_simulations/src/turtlebot3_arm.py
@@ -1,8 +1,6 @@
-import roslib; roslib.load_manifest('my_simulations')
 import rospy
-import moveit_commander
-import moveit_msgs.msg
-import geometry_msgs.msg
+from std_msgs.msg import Float64
+import math
 
 # Keyboard in Linux
 import sys
@@ -19,29 +17,36 @@ def getKey():
         termios.tcsetattr(fd, termios.TCSADRAIN, original_attributes)
     return ch
 
-def arm():
-    group = moveit_commander.MoveGroupCommander("arm")
-    
-    # We can get the joint values from the group and adjust some of the values:
-    joint_goal = group.get_current_joint_values()
-    joint_goal[0] = 0
-    joint_goal[1] = -pi/4
-    joint_goal[2] = 0
-    joint_goal[3] = -pi/2
+def arm(joint1, joint2, joint3, joint4):
+    pub1 = rospy.Publisher('/joint1_position_controller/command', Float64 ,queue_size=10)
+    pub2 = rospy.Publisher('/joint2_position_controller/command', Float64 ,queue_size=10)
+    pub3 = rospy.Publisher('/joint3_position_controller/command', Float64 ,queue_size=10)
+    pub4 = rospy.Publisher('/joint4_position_controller/command', Float64 ,queue_size=10)
+  
+    rate = rospy.Rate(50) # 50hz 
+        
+    position1 = joint1
+    position2 = joint2
+    position3 = joint3
+    position4 = joint4
 
-    # The go command can be called with joint values, poses, or without any
-    # parameters if you have already set the pose or joint target for the group
-    group.go(joint_goal, wait=True)
+    pub1.publish(position1)
+    pub2.publish(position2)
+    pub3.publish(position3)
+    pub4.publish(position4)
+    rate.sleep()
+        
 
-    # Calling ``stop()`` ensures that there is no residual movement
-    group.stop()
-
-
-if __name__='__main__':
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('turtle3_arm',nonymous=True)
+if __name__== '__main__':
     try:
-        arm()
+        rospy.init_node('turtlebot3_arm')
+        while(1):
+            key = getKey()
+            if key == 'z':
+                arm(-0.9,-0.9,0.9,0.9)
+
+            elif key == 'x':
+                arm(0.0,0.0,0.0,0.0)
 
     except rospy.ROSInterruptException:
-        rospy.loginfo("Navigation test finished.")
+        pass
